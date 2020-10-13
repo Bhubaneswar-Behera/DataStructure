a98d17ce91ebb7e69d3f0b1665404bff1ed905c2
Bhubaneswar-Behera <bhubaneswar.behera@gmail.com>
2020-10-14 02:33:19 +0530

    check in

---
+++ src/com/utils/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,47 @@
+package com.utils.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Given nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ *
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+ *
+ * It doesn't matter what values are set beyond the returned length.
+ *
+ *
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] inputArray = {0,0,1,1,1,2,2,3,3,4};
+        List<Integer> outputList  = removeDuplicates(inputArray);
+        printList(outputList);
+    }
+
+    private static List<Integer> removeDuplicates (int[] inputArray) {
+        List<Integer> outputList = new ArrayList();
+        for(int data : inputArray) {
+            if(!outputList.contains(data)){
+                outputList.add(data);
+            }
+        }
+       return outputList;
+    }
+    private static void printList(List<Integer> inputList){
+        inputList.forEach(data -> {
+            System.out.print(" "+data);
+        });
+    }
+}
