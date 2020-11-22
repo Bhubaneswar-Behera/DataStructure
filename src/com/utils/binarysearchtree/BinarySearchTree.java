132001c7c305c5b64cce843b574e8bdf166ab19d
Bhubaneswar-Behera <bhubaneswar.behera@gmail.com>
2020-11-23 02:40:57 +0530

    bst create and insertion

---
+++ src/com/utils/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,89 @@
+package com.utils.binarysearchtree;
+
+
+/**
+ * Class to create Binary Search Tree and display the elements in
+ * InOder , PreOder and PostOrder way
+ *
+ */
+public class BinarySearchTree {
+
+    /**
+     * This method is used to insert elements into the Binary Search Tree
+     *
+     * @param node
+     * @param data
+     * @return
+     */
+    public Node insertElement(Node node , int data){
+
+        if(node == null){
+            return createNode(data);
+        }
+
+        if(data < node.data){
+            node.left = insertElement(node.left,data);
+        } else if(data > node.data){
+            node.right = insertElement(node.right,data);
+        }
+        return node;
+    }
+
+    /**
+     * This method is used to create a node based on a given data
+     *
+     * @param data
+     * @return
+     */
+    private Node createNode(int data){
+        Node node = new  Node();
+        node.data = data;
+        node.left = null;
+        node.right = null;
+
+        return node;
+    }
+
+    /**
+     * This methods is used to display the elements of Binary Search Tree in InOder
+     *
+     * @param node
+     */
+    public void inOrder(Node node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(" " + node.data);
+        inOrder(node.right);
+    }
+
+    /**
+     * This methods is used to display the elements of Binary Search Tree in PreOder
+     *
+     * @param node
+     */
+    public void preOrder(Node node){
+        if(node == null){
+            return;
+        }
+
+        System.out.print(" " + node.data);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * This methods is used to display the elements of Binary Search Tree in PostOder
+     *
+     * @param node
+     */
+    public void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(" " + node.data);
+    }
+}
---
+++ src/com/utils/binarysearchtree/BinarySearchTreeRunner.java
@@ -0,0 +1,33 @@
+package com.utils.binarysearchtree;
+
+
+/**
+ * Runner class for Binary Search Tree insertion
+ *
+ */
+public class BinarySearchTreeRunner {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        Node root = null;
+        //8,3,6,10,4,7,1,14,16
+
+        root = binarySearchTree.insertElement(root,8);
+        root = binarySearchTree.insertElement(root,3);
+        root = binarySearchTree.insertElement(root,6);
+        root = binarySearchTree.insertElement(root,10);
+        root = binarySearchTree.insertElement(root,4);
+        root = binarySearchTree.insertElement(root,7);
+        root = binarySearchTree.insertElement(root,1);
+        root = binarySearchTree.insertElement(root,14);
+        root = binarySearchTree.insertElement(root,16);
+
+        System.out.println("\n InOrder : ");
+        binarySearchTree.inOrder(root);
+        System.out.println("\n PreOrder : ");
+        binarySearchTree.preOrder(root);
+        System.out.println("\n PostOrder : ");
+        binarySearchTree.postOrder(root);
+
+    }
+}
